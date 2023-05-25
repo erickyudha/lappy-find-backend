9356a87cd6f8c845157085c4853a150ff65eb38d
Erick Yudha Pratama Sukku <erickyudha18@gmail.com>
2023-05-25 09:13:50 +0700

    add basic prediction request

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request, abort
+import pickle
+import sklearn
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return render_template('index.html')
+pkl_filename = 'model.pkl'
+with open(pkl_filename, 'rb') as file:
+    model = pickle.load(file)
+
+@app.route("/predict", methods=['POST'])
+def predict_price():
+    try:
+        if request.method == 'POST':
+            PRICE_WEIGHT = 0.6
+            predict_data = request.json
+            price = model.predict([[
+                predict_data['processor_brand'],
+                predict_data['processor_name'],
+                predict_data['ram_gb'],
+                predict_data['ssd'],
+                predict_data['hdd'],
+                predict_data['graphic_card_gb'],
+                predict_data['Touchscreen']
+            ]])[0]
+            return jsonify({
+                "price": int(price * PRICE_WEIGHT)
+            })
+    except:
+        abort(400, 'Bad Request')
 
 if __name__ == '__main__':
   app.run(port=5000)
